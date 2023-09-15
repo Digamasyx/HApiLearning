55f2e80e8312391195059aaa5e27643fcb760863
Opylx <auga1sports@gmail.com>
2023-09-15 14:55:58 -0300

    Lorem

--- src/chars/types/char.ts
+++ src/chars/types/char.ts
@@ -2,14 +2,32 @@
  * @format
  */
 
-export interface BaseChar<T extends CharList> {
+export type BaseChar<T extends CharList> = {
 	name: T;
 	path: CharTypes<T>;
+	element: Elements;
+	stats: Status;
+	skills: Skills;
+	traces: Traces[];
+	eidolons: Eidolons[];
+	materials: AscMateials;
+} & BaseCharIndexable;
+
+interface BaseCharIndexable {
+	[key: string]:
+		| CharList
+		| CharTypes<CharList>
+		| Elements
+		| Status
+		| Skills
+		| Traces[]
+		| Eidolons[]
+		| AscMateials;
 }
 
 export function PullData<T extends CharList>(name: T): BaseChar<T> {
-	let chars = require(`./data/${name}.json`);
-	return chars as BaseChar<typeof name>;
+	let chars = require(`./data/${name}.json`) as BaseChar<typeof name>;
+	return chars;
 }
 
 export function IsInCharList(name: any): name is CharList {
@@ -113,3 +131,103 @@ type Harmony = 'asta' | 'bronya' | 'tingyun' | 'yukong';
 type Hunt = 'dan_heng' | 'seele' | 'sushang' | 'yanqing';
 type Nihility = 'pela' | 'sampo' | 'welt' | 'silver_wolf' | 'kafka' | 'luka';
 type Preservation = 'gepard' | 'march_7th' | 'trailblazer(fire)';
+
+/* Char base types */
+
+type Elements =
+	| 'Fire'
+	| 'Ice'
+	| 'Physical'
+	| 'Lightning'
+	| 'Quantum'
+	| 'Imaginary';
+
+type Status = {
+	[key in StatsKeys]: {
+		baseHp: number[];
+		baseAtk: number[];
+		baseDef: number[];
+	};
+} & { baseSpd: number };
+
+type Skills = {
+	[key in SkillsKeys]: {
+		name: string;
+		type: TypeOfActions;
+		description: string;
+		scalling: number[] | Scalling | Scalling[];
+		tag: Tag;
+		buff: {
+			scalling?: number[];
+			modifier?: string;
+			tag: string;
+		};
+	};
+};
+
+type Traces = {
+	traceBoost: BasicStats;
+	qtd: number;
+	requirement: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 75 | 80;
+};
+
+type Eidolons = {
+	name: string;
+	effect: string;
+	scalling?: number | number[] | Scalling | Scalling[];
+};
+
+type AscMateials = {
+	[key in Range<1, 7>]: {
+		creditCost: number;
+		material:
+			| {
+					name: string;
+					qtd: number;
+			  }
+			| {
+					name: string;
+					qtd: number;
+			  }[];
+	};
+};
+
+/* Keys */
+
+type StatsKeys = 'baseLvl' | 'maxLvl';
+type SkillsKeys = 'basicAtk' | 'skill' | 'ultimate' | 'talent';
+
+/**
+ * Generates a type based on { T start index } & { K end index - 1 }
+ */
+type Range<T extends number, K extends number> = Exclude<
+	Enumerate<K>,
+	Enumerate<T>
+>;
+
+type Enumerate<
+	N extends number,
+	Acc extends number[] = [],
+> = Acc['length'] extends N
+	? Acc[number]
+	: Enumerate<N, [...Acc, Acc['length']]>;
+
+/* Non essential types */
+
+type Scalling = {
+	scalesWith: BasicStats;
+	scalling: number[] | number;
+	modifier?: 'flat' | 'percentage';
+	maxStacks?: number;
+};
+
+type Tag =
+	| 'single_target'
+	| 'blast'
+	| 'enhance'
+	| 'restore'
+	| 'defense'
+	| 'aggro';
+
+type BasicStats = 'hp' | 'def' | 'atk' | 'spd';
+type TypeOfActions = 'dmg' | 'shield' | 'heal' | 'dmg_resist';
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,76 @@
-import { Server, Request, ResponseToolkit } from "@hapi/hapi";
-import { IsInCharList, PullData } from "./chars/types/char";
+/**
+ * @format
+ */
+
+import { Server, Request, ResponseToolkit } from '@hapi/hapi';
+import { IsInCharList, PullData } from './chars/types/char';
 
 const init = async () => {
+	const server: Server = new Server({
+		port: 3000,
+		host: 'localhost',
+	});
 
-    const server: Server = new Server({
-        port: 3000,
-        host: 'localhost'
-    })
+	server.route({
+		method: 'GET',
+		path: '/char/{name}',
+		handler: (req, res) => {
+			try {
+				if (IsInCharList(req.params.name)) {
+					return res.response(PullData(req.params.name)).code(200);
+				} else {
+					return res
+						.response({
+							message: 'Null',
+						})
+						.code(404);
+				}
+			} catch (err) {
+				return res
+					.response({
+						message: err,
+					})
+					.code(500);
+			}
+		},
+	});
 
-    server.route({
-        method: 'GET',
-        path: '/char/{name}',
-        handler: (req, res) => {
-            try {
+	server.route({
+		method: 'GET',
+		path: '/char/{name}/{field}',
+		handler: (req, res) => {
+			try {
+				if (IsInCharList(req.params.name)) {
+					if (typeof req.params.field !== 'string')
+						return res
+							.response({ message: 'Argument must be a string' })
+							.code(400);
+					let temp = req.params.field;
+					return res.response(PullData(req.params.name)[temp]).code(200);
+				} else {
+					return res
+						.response({
+							message: 'Null',
+						})
+						.code(404);
+				}
+			} catch (err) {
+				return res
+					.response({
+						message: err,
+					})
+					.code(500);
+			}
+		},
+	});
 
-                if (IsInCharList(req.params.name)) {
-                    return res.response(PullData(req.params.name)).code(200);
-                } else {
-                    return res.response({
-                        message: "Null" 
-                    }).code(404);
-                }
-            } catch (err) {
-                return res.response({
-                    message: err
-                }).code(500);
-            }
-        }
-    })
+	await server.start();
+	console.log('Server Running on %s', server.info.uri);
+};
 
-    await server.start();
-    console.log('Server Running on %s', server.info.uri);
-}
+process.on('unhandledRejection', err => {
+	console.error(err);
+	process.exit(1);
+});
 
-process.on('unhandledRejection', (err) => {
-    console.error(err);
-    process.exit(1);
-})
-
-init(); 
+init();
